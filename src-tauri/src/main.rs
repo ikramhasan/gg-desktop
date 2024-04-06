8cfca43e3adabce0f8f3b918da8dd9a952859b3d
Ikram Hasan <ikramhasan.dev@gmail.com>
2024-04-06 18:11:20 +0600

    chore: format code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,22 +2,26 @@ use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu
 use tauri_plugin_positioner::{Position, WindowExt};
 
 fn main() {
-   let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
-   let more_apps = CustomMenuItem::new("more_apps".to_string(), "More Apps");
-   let about_me = CustomMenuItem::new("about_me".to_string(), "About Me");
-    let system_tray_menu = SystemTrayMenu::new().add_item(quit).add_item(more_apps).add_item(about_me);
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
+    let more_apps = CustomMenuItem::new("more_apps".to_string(), "More Apps");
+    let about_me = CustomMenuItem::new("about_me".to_string(), "About Me");
+    let system_tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_item(more_apps)
+        .add_item(about_me);
+    
     tauri::Builder::default()
         .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
         .on_window_event(|event| match event.event() {
-         tauri::WindowEvent::Focused(is_focused) => {
-             // detect click outside of the focused window and hide the app
-             if !is_focused {
-                 event.window().hide().unwrap();
-             }
-         }
-         _ => {}
-     })
+            tauri::WindowEvent::Focused(is_focused) => {
+                // detect click outside of the focused window and hide the app
+                if !is_focused {
+                    event.window().hide().unwrap();
+                }
+            }
+            _ => {}
+        })
         .on_system_tray_event(|app, event| {
             tauri_plugin_positioner::on_tray_event(app, &event);
             match event {
@@ -35,19 +39,20 @@ fn main() {
                         window.show().unwrap();
                         window.set_focus().unwrap();
                     }
-                },
+                }
                 SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                  "quit" => {
-                      std::process::exit(0);
-                  },
-                  "more_apps" => {
-                     open::that("https://play.google.com/store/apps/dev?id=4730273045676878035").expect("Failed to open browser");
-                  },
-                  "about_me" => {
-                     open::that("https://ikramhasan.com/").expect("Failed to open browser");
-                  },
-                  _ => {}
-              },
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    "more_apps" => {
+                        open::that("https://play.google.com/store/apps/dev?id=4730273045676878035")
+                            .expect("Failed to open browser");
+                    }
+                    "about_me" => {
+                        open::that("https://ikramhasan.com/").expect("Failed to open browser");
+                    }
+                    _ => {}
+                },
                 _ => {}
             }
         })
